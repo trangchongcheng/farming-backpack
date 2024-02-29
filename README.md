98a42254b27a7d1bdb2c6609dc2faf74a7fab893
trngchongcheng <cheng.cit@gmail.com>
2024-02-29 14:45:24 +0700

    add fee and volume

---
+++ README.md
@@ -0,0 +1,31 @@
+### Script farming dự án sàn cex Backpack (Farming script for BackPack Exchange)
+
+source: https://financewire.com/2024/02/28/backpack-raises-17-million-strategic-series-a-round-led-by-placeholder-vc/
+
+Nếu bạn chưa có tài khoản có thể sử dụng link ref của mình để ủng hộ, cảm ơn rất nhiều:
+https://backpack.exchange/refer/airdrop101
+
+### Thực hiện các bước sau (Follow bellow steps)
+
+1. Cài đặt nodejs + vscode (install nodejs + vscode): https://youtu.be/YMwiiN557yg
+
+2. Cài đặt thư viện (install libraries):
+
+```
+ npm install
+```
+
+3. Run script:
+
+```
+node ./index.js (windows)
+node index.js (macos)
+```
+
+Mod: https://x.com/trangchongcheng
+
+Twitter: https://x.com/airdrop101xyz
+
+Website: https://airdrop101.xyz
+
+Dont foget follow me, thank so much.
--- index.js
+++ index.js
@@ -42,11 +42,47 @@ function getNowFormatDate() {
 let successbuy = 0;
 let sellbuy = 0;
 
+// Cứ mỗi 6 giây đặt lệnh mua/bán 1 lần, ae có thể tăng giảm tùy theo ý thích tối hiểu 3 giây trở lên.
+const delayTime = 6;
+// Điền 0 để tắt tính volumme và fee
+const showVolumeAndFee = 1;
+let totalVolume = 0;
+let totalFee = 0;
 const init = async (client) => {
     try {
-        console.log(`Số lần mua thành công:${successbuy}, Số lần bán thành công:${sellbuy}`);
-        console.log(getNowFormatDate(), "Đang chờ 3 giây...");
-        await delay(3000);
+
+        if (showVolumeAndFee) {
+            // Tính volume và phí 1000 giao dịch gần nhất
+            let histories = await client.FillHistory({
+                symbol: "SOL_USDC",
+                limit: "1000"
+            });
+            if (histories.length) {
+                totalVolume = histories.reduce((pre, next) => {
+                    return pre + (next.quantity * next.price)
+                }, 0)
+                let totalFeeSol = histories.filter(his => his.feeSymbol === 'SOL').reduce((pre, next) => {
+                    return pre + next.fee
+                }, 0) * histories[0].price;
+                let totalFeeUSDC = histories.filter(his => his.feeSymbol === 'USDC').reduce((pre, next) => {
+                    return pre + next.fee
+                }, 0);
+                totalFee = totalFeeSol + totalFeeUSDC
+            }
+        }
+
+        console.log("--------------------------------------------------")
+        console.log("|                                                 |")
+        console.log(`| Số lần mua thành công:${successbuy}, Số lần bán thành công:${sellbuy}|`);
+        console.log(`| Volume 1000 giao dịch gần nhất: ${totalVolume.toFixed(2)}        |`)
+        console.log(`| Phí 1000 giao dịch gần nhất: ${totalFee.toFixed(2)}              |`)
+        console.log("|                                                 |")
+        console.log("--------------------------------------------------")
+
+        console.log("\n\n<--------------- x.com/trangchongcheng ---------------> \n\n")
+
+        console.log(getNowFormatDate(), `Đang chờ ${delayTime} giây...`);
+        await delay(delayTime * 1000);
         console.log(getNowFormatDate(), "Đang lấy thông tin tài khoản...");
         let userbalance = await client.Balance();
         // Kiểm tra số dư USDC trong tài khoản có lớn hơn 5 không
@@ -58,7 +94,7 @@ const init = async (client) => {
         }
     } catch (e) {
         init(client);
-        console.log(getNowFormatDate(), "Đặt hàng thất bại, đang thử lại...");
+        console.log(getNowFormatDate(), "Đặt hàng thất bại, đang thử lại... \n \n");
         await delay(1000);
     }
 }
@@ -74,7 +110,7 @@ const sellfun = async (client) => {
     } else {
         console.log(getNowFormatDate(), "Đơn đặt hàng tài khoản bình thường, không cần hủy đơn đặt hàng");
     }
-    console.log(getNowFormatDate(), "Đang lấy thông tin tài khoản...");
+    console.log(getNowFormatDate(), "\n\n Đang lấy thông tin tài khoản...");
     // Lấy thông tin tài khoản
     let userbalance2 = await client.Balance();
     console.log(getNowFormatDate(), "Thông tin tài khoản:", userbalance2);
@@ -82,7 +118,7 @@ const sellfun = async (client) => {
     // Lấy giá hiện tại
     let { lastPrice: lastPriceask } = await client.Ticker({ symbol: "SOL_USDC" });
     console.log(getNowFormatDate(), "Giá thị trường hiện tại của sol_usdc:", lastPriceask);
-    let quantitys = ((userbalance2.SOL.available / 2) - 0.02).toFixed(2).toString();
+    let quantitys = (userbalance2.SOL.available - 0.02).toFixed(2).toString();
     console.log(getNowFormatDate(), `Đang bán... Bán ${quantitys} SOL`);
     let orderResultAsk = await client.ExecuteOrder({
         orderType: "Limit",
@@ -96,10 +132,10 @@ const sellfun = async (client) => {
     if (orderResultAsk?.status == "Filled" && orderResultAsk?.side == "Ask") {
         console.log(getNowFormatDate(), "Bán thành công");
         sellbuy += 1;
-        console.log(getNowFormatDate(), "Chi tiết đơn hàng:", `Giá bán:${orderResultAsk.price}, Số lượng bán:${orderResultAsk.quantity}, Mã đơn hàng:${orderResultAsk.id}`);
+        console.log(getNowFormatDate(), "Chi tiết đơn hàng:", `Giá bán:${orderResultAsk.price}, Số lượng bán:${orderResultAsk.quantity}, Mã đơn hàng:${orderResultAsk.id}\n\n`);
         init(client);
     } else {
-        console.log(getNowFormatDate(), "Bán thất bại");
+        console.log(getNowFormatDate(), "Bán thất bại ");
         throw new Error("Bán thất bại");
     }
 }
@@ -122,7 +158,6 @@ const buyfun = async (client) => {
     console.log(getNowFormatDate(), "Giá thị trường hiện tại của sol_usdc:", lastPrice);
     console.log(getNowFormatDate(), `Đang mua... Sử dụng ${(userbalance.USDC.available - 2).toFixed(2).toString()} USDC để mua SOL`);
     let quantitys = ((userbalance.USDC.available - 2) / lastPrice).toFixed(2).toString();
-    console.log("1024", quantitys);
     let orderResultBid = await client.ExecuteOrder({
         orderType: "Limit",
         price: lastPrice.toString(),
@@ -134,7 +169,9 @@ const buyfun = async (client) => {
     if (orderResultBid?.status == "Filled" && orderResultBid?.side == "Bid") {
         console.log(getNowFormatDate(), "Đặt hàng thành công");
         successbuy += 1;
-        console.log(getNowFormatDate(), "Chi tiết đơn hàng:", `Giá mua:${orderResultBid.price}, Số lượng mua:${orderResultBid.quantity}, Mã đơn hàng:${orderResultBid.id}`);
+        console.log(getNowFormatDate(), "Chi tiết đơn hàng:", `Giá mua:${orderResultBid.price}, Số lượng mua:${orderResultBid.quantity}, Mã đơn hàng:${orderResultBid.id} \n\n`);
+        console.log("<--------------- x.com/trangchongcheng ---------------> \n\n")
+
         init(client);
     } else {
         console.log(getNowFormatDate(), "Đặt hàng thất bại");
@@ -149,27 +186,3 @@ const buyfun = async (client) => {
     const client = new backpack_client_1.BackpackClient(apisecret, apikey);
     init(client);
 })()
-
-// 卖出
-// client.ExecuteOrder({
-//     orderType: "Limit",
-//     price: "110.00",
-//     quantity: "0.36",
-//     side: "Ask", //卖
-//     symbol: "SOL_USDC",
-//     timeInForce: "IOC"
-// }).then((result) => {
-//     console.log(getNowFormatDate(),result);
-// })
-
-// 买入
-// client.ExecuteOrder({
-//     orderType: "Limit",
-//     price: "110.00",
-//     quantity: "0.36",
-//     side: "Bid", //买
-//     symbol: "SOL_USDC",
-//     timeInForce: "IOC"
-// }).then((result) => {
-//     console.log(getNowFormatDate(),result);
-// })
